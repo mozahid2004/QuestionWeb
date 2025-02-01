ce06af754c3d13768d378292aa110fb895422b6d
Mozahid Izhar <mozahidizhar54@gmail.com>
2025-02-01 12:05:43 +0530

    mobile navigation almost ready

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
 
 const mover = document.querySelector('#mover');
 const mode = document.querySelector('#mode');
+const hamburger = document.querySelector('.hamburger')
+const line = document.querySelector('.line:nth-child(2)')
+const firstLine = document.querySelector(".line");
+const lastLine = document.querySelector(".line:last-of-type"); 
+const mobilelink = document.querySelector('.mobilelink')
+const mnavOpt = document.querySelectorAll('.mnavOpt')
+
+mnavOpt.forEach('')
+
 mode.addEventListener('click', () => {
   if (mover.classList.contains('move')) {
     // Reset to default styles
@@ -12,19 +21,19 @@ mode.addEventListener('click', () => {
   } else {
     // Apply dark mode styles
     mover.classList.add('move');
-    mover.style.backgroundColor = 'black';
-    mode.style.backgroundColor = 'white';
+    mover.style.backgroundColor = 'white';
+    mode.style.backgroundColor = 'black';
     document.body.style.backgroundColor = 'white';
     document.body.style.color = 'black';
+    mobilelink.style.color = 'black'
+    mobilelink.style.backgroundColor = 'rgba(56, 88, 64, .9);'
+    // mnavOpt.style.color = 'black'
   }
 });
 
 // *************Hamburgur*************************
 
-const hamburger = document.querySelector('.hamburger')
-const line = document.querySelector('.line:nth-child(2)')
-const firstLine = document.querySelector(".line");
-const lastLine = document.querySelector(".line:last-of-type"); 
+
 
 
 hamburger.addEventListener('click' , ()=>{
@@ -32,5 +41,6 @@ hamburger.addEventListener('click' , ()=>{
   firstLine.classList.toggle('cross')
   line.classList.toggle('cross')
   lastLine.classList.toggle('cross2')
+  mobilelink.classList.toggle('show')
 
 })
